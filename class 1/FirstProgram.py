b12f9edc8b037d48b48b265135add9c5a7f48d6e
iamfaizan-1 <faizanfaizy634@gmail.com>
2026-06-04 10:19:40 +0500

    python programming

---
+++ class 1/FirstProgram.py
@@ -0,0 +1,25 @@
+print("Hello world")
+
+
+name = "faizan"
+age  = 20
+print("My name is:",name , "and my age is:",age)
+print("My name is:",name)
+print("My age is:",age)
+
+width = 20
+height = 10
+
+area = width*height
+print("The area of rectangle is",area)
+
+print(type(name))
+print(type(age))
+print(type(width))
+
+
+# this is a single line comment
+"""
+This is a multi line comment
+
+f"""
