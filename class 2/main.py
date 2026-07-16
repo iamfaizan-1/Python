25dfd66879628620872f8f174533f0372f5796b2
iamfaizan-1 <faizanfaizy634@gmail.com>
2026-07-16 10:31:16 +0500

    strings

--- class 2/main.py
+++ class 2/main.py
@@ -1,33 +1,33 @@
 #This is my first python code
 
-print("I like pizza")
+# print("I like pizza")
 
-#spaces error
+# #spaces error
 
-if 5>2 :
-    print("5 is greater than 2")
+# if 5>2 :
+#     print("5 is greater than 2")
 
-else:
-    print("5 is not greater")
+# else:
+#     print("5 is not greater")
 
 
     #collection unpack
 
 
-fruits = ["apple","banana","cherry"]   
-x,y,z = fruits
+# fruits = ["apple","banana","cherry"]   
+# x,y,z = fruits
 
-print(x)
-print(y)
-print(z)
+# print(x)
+# print(y)
+# print(z)
 
 
-cities = ["karachi","Lahore","Islamabad"]
+# cities = ["karachi","Lahore","Islamabad"]
 
-a,b,c = cities
-print(a)
-print(b)
-print(c)
+# a,b,c = cities
+# print(a)
+# print(b)
+# print(c)
 
 #escape character
 
@@ -41,3 +41,42 @@ print('who\'s this') #single quotes
 
 a = str(1)
 print(type(a))
+
+
+#strings
+
+print('I told my friend,"Python is my favourite Language"')
+print('This is a string')
+
+name = "faizan"
+
+print(name.title())
+
+
+#even odd function
+
+# num1 = 4
+
+
+# if num1 % 2 == 0:
+#     print("The number is even")
+
+# else: 
+#     print("The number is odd")    
+
+
+#finding largest number
+
+    
+a=20
+b=10
+c=6
+
+if a > b and a>c:
+    print("a is greater")
+
+elif b>a and b>c:
+    print("b is greater")
+
+else:
+    print("c is greater")     
