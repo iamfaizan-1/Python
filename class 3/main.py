d96cf17b6cc3b3384c855eb098963e856d416979
iamfaizan-1 <faizanfaizy634@gmail.com>
2026-07-17 08:30:52 +0500

    class

---
+++ class 3/main.py
@@ -0,0 +1,45 @@
+#class 3
+
+x = "Hello world"
+
+#traffic signal codes
+
+# signal = input("Enter the signal")
+
+# if signal == "green":
+#     print("GO")
+
+# elif signal == "yellow":
+#     print("Ready")
+
+# elif signal == "red":
+#     print("Stop")
+
+# else:
+#     print("invalid signal")
+
+
+# x = ("apple","mango","banana")
+# print(type(x))
+
+# x = range(6)
+# print(x)
+
+# x = {"Name" : "John", "Age" : "21" }
+# print(x)
+
+
+# x = list(range(6))
+# print(x)
+
+
+x  = list(range(1,20))
+
+print(x)
+
+x = list(range(0,51,2))
+print(x)
+
+if 5> 2:
+ print('5 is greater than 2')
+ print("5 is greater than 2")
