653d3f6ca58abff34a30380adda9c718b11303c6
iamfaizan-1 <faizanfaizy634@gmail.com>
2026-07-19 20:12:07 +0500

    sets

---
+++ class 5/main.py
@@ -0,0 +1,42 @@
+# Data structures
+
+#1.List
+
+list = [1,2,3,4,5]
+
+print(*list)
+
+print(list,sep = " ")
+
+list.insert(6,6)
+
+list.insert(len(list),7)
+
+list.append(8)
+
+list.extend([9,10])
+print(list)
+
+
+#del functions of list
+
+list.pop(4)
+del list[3]
+
+print(*list)
+
+
+# 2.tuple
+
+tuple = ("str",1,4,6)
+print(tuple.count(6)) #count is used to find if the element is present in tuple
+
+#sets
+
+s1 = {1,2,3,4}
+s1.add(5)
+print(s1)
+s1.remove(5)
+print(s1)
+
+
