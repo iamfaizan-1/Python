c4ed8632f2df3875d3193187b0c2ee2eba96ba00
iamfaizan-1 <faizanfaizy634@gmail.com>
2026-07-20 10:45:47 +0500

    dictionaries

--- class 5/main.py
+++ class 5/main.py
@@ -2,41 +2,64 @@
 
 #1.List
 
-list = [1,2,3,4,5]
+# list = [1,2,3,4,5]
 
-print(*list)
+# print(*list)
 
-print(list,sep = " ")
+# print(list,sep = " ")
 
-list.insert(6,6)
+# list.insert(6,6)
 
-list.insert(len(list),7)
+# list.insert(len(list),7)
 
-list.append(8)
+# list.append(8)
 
-list.extend([9,10])
-print(list)
+# list.extend([9,10])
+# print(list)
 
 
 #del functions of list
 
-list.pop(4)
-del list[3]
+# list.pop(4)
+# del list[3]
 
-print(*list)
+# print(*list)
 
 
 # 2.tuple
 
-tuple = ("str",1,4,6)
-print(tuple.count(6)) #count is used to find if the element is present in tuple
+# tuple = ("str",1,4,6)
+# print(tuple.count(6)) #count is used to find if the element is present in tuple
 
 #sets
 
-s1 = {1,2,3,4}
-s1.add(5)
-print(s1)
-s1.remove(5)
-print(s1)
+# s1 = {1,2,3,4}
+# s1.add(5)
+# print(s1)
+# s1.remove(5)
+# print(s1)
+
+#dictionaries
+
+# dic = {"name":"faizan",1:5,2:"Text"}
 
 
+
+# print(dic)
+
+# dic[1] = "one"
+
+# del dic[1]
+
+# print(dic) 
+
+# for key, value in dic.items():
+  
+#   print("key = "+str(key)+" value = "+str(value))
+
+
+
+my_dict = {"name": "Alice", "age": 30}
+my_dict["city"] = "New York"
+print(my_dict["age"])
+print(my_dict)
