7677844f1b430f3c4bb9dab7b23b4195ab1e2fa4
iamfaizan-1 <faizanfaizy634@gmail.com>
2026-07-21 10:43:19 +0500

    args and kwargs

---
+++ class 6/main.py
@@ -0,0 +1,25 @@
+""" Args  """
+
+
+def sumOf(*args):
+    sum = 0
+
+    for x in args:
+        sum+=x
+        
+    return sum
+    
+# print(sumOf(1,2,9,7,5))
+
+
+
+""" kwargs """
+
+def totalOf(**kwargs):
+    sum = 0
+
+    for x, y in kwargs.items():
+        sum+=y
+    return sum
+
+print(totalOf(coffee = 3, cake = 4, juice = 2))
